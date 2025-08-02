a1bb50a24659638f099d9285875fbb8f25260701
Dieguidev <diegogaraycullas@gmail.com>
2025-08-02 10:40:33 -0500

    adding prisma

--- backend-devtree-clon/src/app.module.ts
+++ backend-devtree-clon/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [],
   controllers: [],
   providers: [],
 })
